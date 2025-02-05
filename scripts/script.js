8d4ce5c60eb90629b88f5a340548187b1e8d2b21
Zharick <zharicklondos@gmail.com>
2025-02-05 18:27:49 -0500

    ajustes al responsive del top background

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// document.addEventListener("scroll", function () {
+//     let menu = document.querySelector(".menu-container");
+//     if (window.scrollY > 200) {
+//         menu.classList.remove("menu-container")
+//         menu.classList.add("menu-scroll"); // Aplica el blur cuando se hace scroll
+//     } else {
+//         menu.classList.remove("menu-scroll"); // Lo quita cuando está arriba
+//         menu.classList.add("menu-container")
+//     }
+// });
