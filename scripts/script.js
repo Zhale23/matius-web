8b8065565ddad9bcf563cf29649e5d7c75348ff9
Zharick <zharicklondos@gmail.com>
2025-02-07 14:53:20 -0500

    se realizaron añadiduras al codigo js para corregir el error en el cual el menu offcanvas al seleccionar la opcion, se devolvía. con esto se le da fin al proyecto.

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,40 @@ document.addEventListener("scroll", function () {
     //     menu.classList.remove("menu-scroll"); 
     // }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var offcanvasNavbar = document.getElementById('offcanvasNavbar');
+    
+    // Desactivar scroll al abrir el offcanvas
+    offcanvasNavbar.addEventListener('shown.bs.offcanvas', function () {
+        document.body.style.overflow = 'hidden';
+    });
+    
+    offcanvasNavbar.addEventListener('hidden.bs.offcanvas', function () {
+        document.body.style.overflow = 'auto';
+    });
+
+    // Cerrar offcanvas y navegar a la sección al hacer clic en un enlace
+    var navLinks = document.querySelectorAll('.offcanvas-body .nav-link');
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
+            
+            var targetSection = document.querySelector(this.getAttribute('href'));
+            
+            var bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvasNavbar);
+            if (bsOffcanvas) {
+                bsOffcanvas.hide();
+            }
+            
+            if (targetSection) {
+                // Esperar a que el offcanvas se cierre antes de desplazarse
+                setTimeout(function() {
+                    targetSection.scrollIntoView({ behavior: 'smooth' });
+                }, 300); // Ajustar el tiempo si es necesario
+            }
+            
+            document.body.style.overflow = 'auto';
+        });
+    });
+});
